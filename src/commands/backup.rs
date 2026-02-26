0ae09fb3280fb7a8b75c74a838ec3e482ea56ee8
Josh Priddle <jpriddle@me.com>
2026-02-26 12:23:25 -0500

    fixup

--- src/commands/backup.rs
+++ src/commands/backup.rs
@@ -60,7 +60,10 @@ pub fn list(
         })
         .collect();
 
-    print_table(vec!["ID", "Type", "Scope", "Status", "Description", "Created"], rows);
+    print_table(
+        vec!["ID", "Type", "Scope", "Status", "Description", "Created"],
+        rows,
+    );
 
     if let Some((current, last, total)) = extract_pagination(&response) {
         print_pagination(current, last, total);
